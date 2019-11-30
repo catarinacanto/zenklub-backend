24cdef6ec9e340794df762edcfd6d434189fad2a
catarinacanto <catarinaccanto@gmail.com>
2019-11-30 17:54:10 +0000

    Add files via upload

---
+++ zenklub/src/main/java/com/ccanto/zenklub/ZenklubApplication.java
@@ -0,0 +1,13 @@
+package com.ccanto.zenklub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenklubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenklubApplication.class, args);
+	}
+
+}
---
+++ zenklub/src/test/java/com/ccanto/zenklub/ZenklubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccanto.zenklub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenklubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
