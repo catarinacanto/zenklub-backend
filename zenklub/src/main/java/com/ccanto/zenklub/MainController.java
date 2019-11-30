facfbb22e0d8f53c2616b643d2a2668f391371d9
Catarina Canto <catarinaccanto@gmail.com>
2019-11-30 18:38:03 +0000

    Added professional entity and db

--- zenklub/src/main/java/com/ccanto/zenklub/MainController.java
+++ zenklub/src/main/java/com/ccanto/zenklub/MainController.java
@@ -1,11 +1,11 @@
 package com.ccanto.zenklub;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @CrossOrigin
---
+++ zenklub/src/main/java/com/ccanto/zenklub/professional/Professional.java
@@ -0,0 +1,161 @@
+package com.ccanto.zenklub.professional;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Table(name = "zk_professional")
+@Entity
+public class Professional {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String skill;
+
+    @Column(nullable = false)
+    private String skill1;
+
+    @Column(nullable = false)
+    private String skill2;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Long minutesPerSession;
+
+    @Column(nullable = false)
+    private Date createDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public String getSkill1() {
+        return skill1;
+    }
+
+    public void setSkill1(String skill1) {
+        this.skill1 = skill1;
+    }
+
+    public String getSkill2() {
+        return skill2;
+    }
+
+    public void setSkill2(String skill2) {
+        this.skill2 = skill2;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getMinutesPerSession() {
+        return minutesPerSession;
+    }
+
+    public void setMinutesPerSession(Long minutesPerSession) {
+        this.minutesPerSession = minutesPerSession;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Professional{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", description='" + description + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", skill='" + skill + '\'' +
+                ", skill1='" + skill1 + '\'' +
+                ", skill2='" + skill2 + '\'' +
+                ", price=" + price +
+                ", minutesPerSession=" + minutesPerSession +
+                ", createDate=" + createDate +
+                '}';
+    }
+}
